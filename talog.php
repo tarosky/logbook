e682de564b92d66110cf15a3290c1732f860395a
miya0001 <miya@wpist.me>
2017-10-27 19:04:39 +0900

    first commit

---
+++ talog.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Talog
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     talog
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Talog
+ */
