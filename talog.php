248584240f4109cadaeb30e8bef990b39840fe34
miya0001 <miya0001@users.noreply.github.com>
2017-10-30 02:27:37 +0900

    add auto updater

--- talog.php
+++ talog.php
@@ -7,12 +7,23 @@
  * Author URI:
  * Text Domain:     talog
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         nightly
  *
  * @package         Talog
  */
 
 require_once dirname( __FILE__ ) . '/vendor/autoload.php';
+
+add_action( 'init', 'activate_autoupdate' );
+
+function activate_autoupdate() {
+	$plugin_slug = plugin_basename( __FILE__ ); // e.g. `hello/hello.php`.
+	$gh_user = 'tarosky';                      // The user name of GitHub.
+	$gh_repo = 'talog';       // The repository name of your plugin.
+
+	// Activate automatic update.
+	new Miya\WP\GH_Auto_Updater( $plugin_slug, $gh_user, $gh_repo );
+}
 
 $post_type = new Talog\Post_Type();
 $post_type->register();
