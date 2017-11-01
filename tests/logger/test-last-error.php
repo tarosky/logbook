bf69a3f7817304345b1c3fa7dee2f04e39065cc1
miya0001 <miya0001@users.noreply.github.com>
2017-11-02 03:14:00 +0900

    add tests for last_error

--- tests/logger/test-last-error.php
+++ tests/logger/test-last-error.php
@@ -35,4 +35,36 @@ class Talog_Last_Error_Test extends \WP_UnitTestCase
 		$result = Talog\Logger\Last_Error::get_a_part_of_file( $file );
 		$this->assertSame( 16, count( explode( "\n", trim( $result ) ) ) );
 	}
+
+	public function test_log()
+	{
+		$obj = new Talog\Logger\Last_Error();
+
+		echo @$e; // error;
+
+		$GLOBALS['wp_current_filter'] = array( 'test' ); // Force `test` hook.
+
+		$log = new Talog\Log();
+		$obj->log( $log, array() );
+
+		$res = $log->get_log();
+		$this->assertSame( 'Undefined variable: e', $res->title );
+		$this->assertSame( __FILE__, $res->meta['error']['file'] );
+	}
+
+	public function test_admin()
+	{
+		$obj = new Talog\Logger\Last_Error();
+		$post = $this->factory()->post->create_and_get( array(
+			'post_content' => '',
+		) );
+
+		echo @$e; // error;
+		$error = error_get_last();
+
+		$this->assertFalse( !! $post->post_content );
+
+		$obj->admin( $post, array( 'error' => $error, 'error-file' => 'file' ) );
+		$this->assertTrue( !! $post->post_content );
+	}
 }
