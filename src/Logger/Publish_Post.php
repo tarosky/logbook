b23a706ba09709e58bf0205b02699593b4d4d786
miya0001 <miya0001@users.noreply.github.com>
2017-11-03 07:01:07 +0900

    update publish_post

--- src/Logger/Publish_Post.php
+++ src/Logger/Publish_Post.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace Talog\Logger;
-use Talog\Log;
 use Talog\Logger;
 
 class Publish_Post extends Logger
@@ -15,31 +14,22 @@ class Publish_Post extends Logger
 	/**
 	 * Set the properties to the `Talog\Log` object for the log.
 	 *
-	 * @param Log    $log             An instance of `Talog\Log`.
 	 * @param mixed  $additional_args An array of the args that was passed from WordPress hook.
 	 */
 	public function log( $additional_args )
 	{
 		list( $post_id, $post ) = $additional_args;
-		$title = '#' . $post_id . ' "' . $post->post_title . '" was published.';
-		$log->set_title( $title );
-		$log->update_meta( 'post_title', $post->post_title );
-		$log->update_meta( 'post_id', $post_id );
-	}
 
-	/**
-	 * Set the properties to `\WP_Post` for the admin.
-	 *
-	 * @param \WP_Post $post     The post object.
-	 * @param array   $post_meta The post meta of the `$post`.
-	 * @return \WP_Post The `\WP_Post` object.
-	 */
-	public function admin( \WP_Post $post, $post_meta )
-	{
-		$post->post_content = $this->get_table( array(
-			'ID' => esc_html( $post_meta['post_id'] ),
-			'Title' => esc_html( $post_meta['post_title'] ),
-			'URL' => esc_url( get_the_permalink( $post_meta['post_id'] ) )
+		$content = $this->get_table( array(
+			'ID' => esc_html( $post_id ),
+			'Title' => esc_html( $post->post_title  ),
+			'URL' => sprintf(
+				'<a href="%1$s">%1$s</a>',
+				esc_url( get_the_permalink( $post_id ) )
+			)
 		) );
+
+		$this->set_title( '#' . $post_id . ' "' . $post->post_title . '" was published.' );
+		$this->add_content( 'Summary', $content );
 	}
 }
--- talog.php
+++ talog.php
@@ -50,7 +50,7 @@ function plugins_loaded() {
 		'Talog\Logger\Delete_Post',
 		'Talog\Logger\Last_Error',
 		'Talog\Logger\Post_Updated',
-//		'Talog\Logger\Publish_Post',
+		'Talog\Logger\Publish_Post',
 //		'Talog\Logger\WP_Delete_File',
 //		'Talog\Logger\WP_Login',
 	) );
