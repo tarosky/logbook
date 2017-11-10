9f9b9c441fea7093080dd852e0ee939eb77dbb7c
miya0001 <miya@wpist.me>
2017-11-10 16:19:15 +0900

    bug fix for tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -22,7 +22,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/loggbook.php';
+	require dirname( dirname( __FILE__ ) ) . '/logbook.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
